4a89a0abbadd3c3c0a3b153011b4948fee2d7d0e
JamilsonMello <jamilsonmellow@gmail.com>
2020-04-26 15:05:23 -0300

    final commit

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,9 +1,16 @@
+/* eslint-disable prettier/prettier */
 import Transaction from '../models/Transaction';
 
 interface Balance {
   income: number;
   outcome: number;
   total: number;
+}
+
+interface CreateTransactions {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
 }
 
 class TransactionsRepository {
@@ -14,15 +21,37 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const { income, outcome, total } =  this.transactions.reduce(
+      (acumulator: Balance, element: CreateTransactions) => {
+
+        acumulator.income += element.type === 'income' ? element.value : 0;
+
+        acumulator.outcome += element.type === 'outcome' ? element.value : 0;
+
+        acumulator.total = acumulator.income - acumulator.outcome;
+
+        return acumulator;
+      },
+      { income: 0, outcome: 0, total: 0},
+    );
+
+    return {
+      income,
+      outcome,
+      total
+    };
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactions): Transaction {
+    const transactions = new Transaction({ title, value, type });
+
+    this.transactions.push(transactions);
+
+    return transactions;
   }
 }
 
